f9b482cc114aec2bd8f44960a43777fa1268cf56
Rahul Gahlot <36023973+starinfinity@users.noreply.github.com>
2019-09-27 18:13:13 -0400

    Add files via upload

--- randomnumbergen.py
+++ randomnumbergen.py
@@ -16,14 +16,20 @@ while i == True:
     if bin != "B":
         print("Do you want to generate the seed too")
         a = input("Press Y for yes N for no\n").upper()
-        min = int(input("Please enter the minimum range of Number \t\t"))
-        max = int(input("Please enter the Maximum range of Number \t\t"))
+        min = 0
+        max = 1000
+        print("Default range is 0-1000, to update please enter the range ")
+        min = input("Please enter the minimum range of Number \t\t")
+        max = input("Please enter the Maximum range of Number \t\t")
         now = datetime.datetime.now()
         seed = now.year*now.month*now.day*now.hour*now.minute*now.second*now.microsecond
         if a == "Y":
             print("Seed is \t\t\t\t\t", seed)
         rand = random.seed(seed)
-        randomint = random.randint(min, max)
+        try:
+            randomint = random.randint(int(min), int(max))
+        except Exception as e:
+            randomint = random.randint(0, 1000)
         print("Random Number is \t\t\t\t\t", randomint)
     else:
         leng = int(input("Please enter the length of Random binary number to be generated\t"))
@@ -35,6 +41,6 @@ while i == True:
             randint = random.randint(0,1)
             res = str(res)+str(randint)
         print("Random Binary number of length %d is %s" %(leng,res))
-    b = input("To generate another random number press Y else press any key\n").upper()
-    if b != "Y":
+    b = input("To generate another random number press any key else press Exit \n").upper()
+    if b == "EXIT":
         i = False
