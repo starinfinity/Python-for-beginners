960034e9f39c377f6f61128a8c13b9d3f574202b
Rahul Gahlot <36023973+starinfinity@users.noreply.github.com>
2019-09-27 17:57:18 -0400

    Add files via upload

--- randomnumbergen.py
+++ randomnumbergen.py
@@ -11,16 +11,30 @@ while i == True:
     print("  / /_/ / __ `/ __ \/ __  / __ \/ __ `__ \   /  |/ / / / / __ `__ \/ __ \/ _ \/ ___/  / / __/ _ \/ __ \/ _ \/ ___/ __ `/ __/ __ \/ ___/")
     print(" / _, _/ /_/ / / / / /_/ / /_/ / / / / / /  / /|  / /_/ / / / / / / /_/ /  __/ /     / /_/ /  __/ / / /  __/ /  / /_/ / /_/ /_/ / /    ")
     print("/_/ |_|\__,_/_/ /_/\__,_/\____/_/ /_/ /_/  /_/ |_/\__,_/_/ /_/ /_/_.___/\___/_/      \____/\___/_/ /_/\___/_/   \__,_/\__/\____/_/     ")
-
-    print("Do you want to generate the seed too")
-    a = input("Press Y for yes N for no\n").upper()
-    now = datetime.datetime.now()
-    seed = now.year*now.month*now.day*now.hour*now.minute*now.second*now.microsecond
-    if a == "Y":
-        print(seed)
-    rand = random.seed(seed)
-    randomint = random.randint(0,1000)
-    print("random number is ", randomint)
+    print("To generate a binary number please press B")
+    bin = input("To generate natural number press any other key\n").upper()
+    if bin != "B":
+        print("Do you want to generate the seed too")
+        a = input("Press Y for yes N for no\n").upper()
+        min = int(input("Please enter the minimum range of Number \t\t"))
+        max = int(input("Please enter the Maximum range of Number \t\t"))
+        now = datetime.datetime.now()
+        seed = now.year*now.month*now.day*now.hour*now.minute*now.second*now.microsecond
+        if a == "Y":
+            print("Seed is \t\t\t\t\t", seed)
+        rand = random.seed(seed)
+        randomint = random.randint(min, max)
+        print("Random Number is \t\t\t\t\t", randomint)
+    else:
+        leng = int(input("Please enter the length of Random binary number to be generated\t"))
+        res = " "
+        for j in range(leng):
+            now = datetime.datetime.now()
+            seed = now.year*now.month*now.day*now.hour*now.minute*now.second*now.microsecond
+            rand = random.seed(seed)
+            randint = random.randint(0,1)
+            res = str(res)+str(randint)
+        print("Random Binary number of length %d is %s" %(leng,res))
     b = input("To generate another random number press Y else press any key\n").upper()
     if b != "Y":
         i = False
